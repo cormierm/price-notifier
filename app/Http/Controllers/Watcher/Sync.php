969eabb4060be428cfc5b5246476fbbc27003900
Matt Cormier <m.cormier@vehikl.com>
2020-08-14 10:12:16 -0400

    Updates watcher sync to use job

--- app/Http/Controllers/Watcher/Sync.php
+++ app/Http/Controllers/Watcher/Sync.php
@@ -3,37 +3,19 @@
 namespace App\Http\Controllers\Watcher;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\UpdateWatcher;
 use App\Utils\HtmlFetcher;
-use App\Utils\HtmlParser;
-use App\Utils\PriceHelper;
 use App\Watcher;
-use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 
 class Sync extends Controller
 {
     public function __invoke(Watcher $watcher, HtmlFetcher $fetcher)
     {
-        $html = $fetcher->getHtmlFromUrl($watcher->url);
-
-        if ($watcher->query_type === 'class') {
-            $query = '//*[@class="' . $watcher->query . '"]';
-        } elseif ($watcher->query_type === 'id') {
-            $query = '//*[@id="' . $watcher->query . '"]';
-        } else {
-            $query = $watcher->query;
-        }
-
-        $parser = new HtmlParser($html);
-        $text = $parser->nodeValueByXPathQuery($query);
-
-        $watcher->update([
-            'last_sync' => Carbon::now(),
-            'value' => PriceHelper::numbersFromText($text),
-        ]);
+        UpdateWatcher::dispatch($watcher);
 
         return new JsonResponse([
-            'watcher' => $watcher
+            'watcher' => $watcher->fresh()
         ]);
     }
 }
