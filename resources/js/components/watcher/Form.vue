cbc12b0c97b14ac102801f17aec6b3f0d55d1ec2
Matt Cormier <m.cormier@vehikl.com>
2020-08-16 11:22:16 -0400

    Adds test feature to watcher form

--- resources/js/components/watcher/Form.vue
+++ resources/js/components/watcher/Form.vue
@@ -1,54 +1,78 @@
 <template>
-    <form class="wrapper-form" action="">
-        <b-field
-            label="Name"
-            :type="formErrors['name'] ? 'is-danger' : 'is-default'"
-            :message="formErrors['name']"
-        >
-            <b-input v-model="name" placeholder="Product Name"></b-input>
-        </b-field>
+    <div>
+        <div v-if="testResults">
+            <b-message v-if="testResults.error" title="Danger" type="is-danger" aria-close-label="Close message">
+                {{ testResults.error }}
+            </b-message>
+            <b-message v-else title="Success" type="is-success" aria-close-label="Close message">
+                Value: {{ testResults.value }}<br>
+                Value before formatting: {{ testResults.raw_value }}<br>
+                Title: {{ testResults.title }}<br>
+            </b-message>
+        </div>
 
-        <b-field
-            label="Url"
-            :type="formErrors['url'] ? 'is-danger' : 'is-default'"
-            :message="formErrors['url']"
-        >
-            <b-input v-model="url" placeholder="https://www.example.com/product.html"></b-input>
-        </b-field>
+        <form class="wrapper-form" action="">
+            <b-field
+                label="Url"
+                :type="formErrors['url'] ? 'is-danger' : 'is-default'"
+                :message="formErrors['url']"
+            >
+                <b-input v-model="url" placeholder="https://www.example.com/product.html"></b-input>
+            </b-field>
 
-        <b-field
-            label="XPath Query"
-            :type="formErrors['query'] ? 'is-danger' : 'is-default'"
-            :message="formErrors['query']"
-        >
-            <b-input v-model="query"></b-input>
-        </b-field>
+            <b-field
+                label="Name"
+                :type="formErrors['name'] ? 'is-danger' : 'is-default'"
+                :message="formErrors['name']"
+            >
+                <b-input v-model="name" placeholder="Product Name"></b-input>
+            </b-field>
 
-        <b-field
-            label="Interval"
-            :type="formErrors['interval_id'] ? 'is-danger' : 'is-default'"
-            :message="formErrors['interval_id']"
-        >
-            <b-select placeholder="Select an interval" v-model="interval">
-                <option
-                    v-for="option in intervals"
-                    :value="option.id"
-                    :key="option.id">
-                    {{ option.name }}
-                </option>
-            </b-select>
-        </b-field>
+            <b-field
+                label="Value XPath Query"
+                :type="formErrors['query'] ? 'is-danger' : 'is-default'"
+                :message="formErrors['query']"
+            >
+                <b-input v-model="xpathValue"></b-input>
+            </b-field>
 
-        <b-field
-            label="Alert Value"
-            :type="formErrors['alert_value'] ? 'is-danger' : 'is-default'"
-            :message="formErrors['alert_value']"
-        >
-            <b-input v-model="alertValue" placeholder="5.00"></b-input>
-        </b-field>
+            <b-field
+                label="Title XPath Query"
+                :type="formErrors['xpath_title'] ? 'is-danger' : 'is-default'"
+                :message="formErrors['xpath_title']"
+            >
+                <b-input v-model="xpathTitle"></b-input>
+            </b-field>
 
-        <b-button type="is-info" @click="submit" :loading="loading">{{ type }}</b-button>
-    </form>
+            <b-field
+                label="Interval"
+                :type="formErrors['interval_id'] ? 'is-danger' : 'is-default'"
+                :message="formErrors['interval_id']"
+            >
+                <b-select placeholder="Select an interval" v-model="interval">
+                    <option
+                        v-for="option in intervals"
+                        :value="option.id"
+                        :key="option.id">
+                        {{ option.name }}
+                    </option>
+                </b-select>
+            </b-field>
+
+            <b-field
+                label="Alert Value"
+                :type="formErrors['alert_value'] ? 'is-danger' : 'is-default'"
+                :message="formErrors['alert_value']"
+            >
+                <b-input v-model="alertValue" placeholder="5.00"></b-input>
+            </b-field>
+        </form>
+
+        <div class="buttons">
+            <b-button :loading="loading" @click="check">Test</b-button>
+            <b-button type="is-info" @click="submit" :loading="loading">{{ type }}</b-button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -73,19 +97,21 @@ export default {
             this.id = this.watcher.id;
             this.name = this.watcher.name;
             this.interval = this.watcher.interval_id;
-            this.query = this.watcher.query;
+            this.xpathValue = this.watcher.query;
             this.url = this.watcher.url;
             this.alertValue = this.watcher.alert_value;
         }
     },
     data() {
         return {
+            testResults: null,
             loading: false,
             id: null,
             name: '',
             interval: null,
             alertValue: '',
-            query: '//div[@id="price"]',
+            xpathValue: '//span[@id="price"]',
+            xpathTitle: '//span[@class="title"]',
             url: '',
             formErrors: {},
         };
@@ -108,7 +134,8 @@ export default {
                 name: this.name,
                 interval_id: this.interval,
                 url: this.url,
-                query: this.query,
+                query: this.xpathValue,
+                xpath_title: this.xpathTitle,
                 alert_value: this.alertValue
             }).then(() => {
                 window.location = '/home';
@@ -125,6 +152,28 @@ export default {
                 }
             });
         },
+        check() {
+            this.loading = true;
+            this.testResults = null;
+            axios.post('/watcher/check', {
+                _token: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                url: this.url,
+                xpath_value: this.xpathValue,
+                xpath_title: this.xpathTitle,
+            }).then(({data}) => {
+                this.testResults = data;
+            }).catch((err) => {
+                if (err.response.status === 400) {
+                    this.testResults = err.response.data;
+                } else {
+                    this.testResults = {
+                        error:  err,
+                    }
+                }
+            }).finally(() => {
+                this.loading = false;
+            });
+        },
         update() {
             axios.put(`/watcher/${this.id}`, {
                 _token: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
@@ -132,7 +181,8 @@ export default {
                 name: this.name,
                 interval_id: this.interval,
                 url: this.url,
-                query: this.query,
+                query: this.xpathValue,
+                xpath_title: this.xpathTitle,
                 alert_value: this.alertValue
             }).then(() => {
                 window.location = '/home';
@@ -157,5 +207,11 @@ export default {
     .wrapper-form {
         display: flex;
         flex-direction: column;
+    }
+
+    .buttons {
+        margin-top: 20px;
+        display: flex;
+        justify-content: space-between;
     }
 </style>
