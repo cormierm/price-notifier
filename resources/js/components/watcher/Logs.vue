20d5340711f50a84729bba45836bb585f01da122
Matt Cormier <m.cormier@vehikl.com>
2020-08-17 21:23:00 -0400

    Adds logs and details to watcher show view

---
+++ resources/js/components/watcher/Logs.vue
@@ -0,0 +1,59 @@
+<template>
+    <b-table
+        :columns="columns"
+        :data="logs"
+        :row-class="(row) => row.error && 'is-danger'"
+        default-sort="created_at"
+        default-sort-direction="desc"
+    ></b-table>
+</template>
+
+<script>
+export default {
+    name: "watcher-logs",
+    props: {
+        logs: {
+            type: Array,
+            default: () => ([])
+        }
+    },
+    data() {
+        return {
+            columns: [
+                {
+                    field: 'created_at',
+                    label: 'Created',
+                    sortable: true
+                },
+                {
+                    field: 'formatted_value',
+                    label: 'Formatted Value',
+                },
+                {
+                    field: 'raw_value',
+                    label: 'Raw Value',
+                },
+                {
+                    field: 'duration',
+                    label: 'Duration (ms)',
+                },
+                {
+                    field: 'error',
+                    label: 'Error',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+<style>
+    tr.is-danger {
+        background: #ffc4c4;
+        color: #ff0000;
+    }
+</style>
--- resources/views/watcher/show.blade.php
+++ resources/views/watcher/show.blade.php
@@ -2,8 +2,18 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            {{ $watcher->name }}
-        </div>
+        <h1 class="title">Watcher Details</h1>
+
+        Name: <strong>{{ $watcher->name }}</strong><br>
+        Url: {{ $watcher->url }}<br>
+        Last Synced: {{ $watcher->last_sync }}<br>
+        Initial: {{ $watcher->interval->name }}<br>
+        Original Price: {{ $watcher->initial_value }}<br>
+        Current Price: {{ $watcher->initial_value }}<br>
+        Alert Price: {{ $watcher->initial_value }}<br>
+        XPath Query Price: {{ $watcher->query }}
+
+        <h2 class="mt-3 subtitle">Api logs</h2>
+        <watcher-logs :logs="{{ $watcher->logs }}"></watcher-logs>
     </div>
 @endsection
