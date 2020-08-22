759f6b4aa07a33961a0b7f40fc77653f25f08f3b
Matt Cormier <m.cormier@vehikl.com>
2020-08-21 21:28:51 -0400

    Adds buefy navbar

---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item class="logo" tag="router-link" :to="{ path: '/home' }">
+                PriceWatcher
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="/home">
+                Watchers
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-dropdown :label="username">
+                <b-navbar-item @click="logout">
+                    Logout
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+    props: {
+        username: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        logout() {
+            axios.post('/logout').then(() => {
+                window.location = '/';
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .logo {
+        font-size: 1.2em;
+        font-weight: 500;
+    }
+</style>
