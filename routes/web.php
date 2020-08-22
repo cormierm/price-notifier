b3de3bbf4aa3513072927ac8ead2486fd8daa3d1
Matt Cormier <m.cormier@vehikl.com>
2020-08-21 20:54:17 -0400

    Disables registration

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::middleware('auth')->group(function() {
     Route::get('/home', 'HomeController@index')->name('home');
