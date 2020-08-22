85ef4eae098bb7985fa8627fe1e5bf09e2f4ab1b
Matt Cormier <m.cormier@vehikl.com>
2020-08-21 21:49:46 -0400

    Adds max values to validation rules

--- app/Http/Requests/Watcher/StoreRequest.php
+++ app/Http/Requests/Watcher/StoreRequest.php
@@ -10,10 +10,10 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|min:2',
-            'url' => 'required|url',
-            'query'  => 'required|string|min:2',
-            'xpath_name' => 'required|string|min:2',
+            'name' => 'required|string|min:2|max:191',
+            'url' => 'required|url|max:191',
+            'query'  => 'required|string|min:2|max:191',
+            'xpath_name' => 'required|string|min:2|max:191',
             'interval_id' => 'nullable|exists:intervals,id',
             'alert_value' => 'nullable|numeric',
             'client' => 'required|in:' . implode(',', HtmlFetcher::CLIENTS),
--- app/Http/Requests/Watcher/UpdateRequest.php
+++ app/Http/Requests/Watcher/UpdateRequest.php
@@ -10,10 +10,10 @@ class UpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'nullable|string|min:2',
-            'query' => 'nullable|string|min:2',
-            'url' => 'nullable|url',
-            'xpath_name' => 'nullable|string|min:2',
+            'name' => 'nullable|string|min:2|max:191',
+            'query' => 'nullable|string|min:2|max:191',
+            'url' => 'nullable|url|max:191',
+            'xpath_name' => 'nullable|string|min:2|max:191',
             'interval_id' => 'nullable|exists:intervals,id',
             'alert_value' => 'nullable|numeric',
             'client' => 'nullable|in:' . implode(',', HtmlFetcher::CLIENTS),
