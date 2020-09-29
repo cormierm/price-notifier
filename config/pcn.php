a23762a18921cbfa9a77056a44abfbc8e1158849
Matt Cormier <m.cormier@vehikl.com>
2020-09-29 08:35:07 -0400

    Add env for fetcher user agent

--- config/pcn.php
+++ config/pcn.php
@@ -6,7 +6,7 @@ return [
         'npm_bin' => env('BROWSERSHOT_NPM_BIN', '/usr/bin/npm'),
     ],
     'fetcher' => [
-        'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+        'user_agent' => env('FETCHER_USER_AGENT', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'),
         'timeout' => 10,
         'delay' => env('FETCHER_DELAY', 30),
         'fetch_null_regions' => env('FETCH_NULL_REGION_WATCHER', false),
