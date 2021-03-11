9cf7e74d25d24b34dd7fea32218a134812b28d29
Matt Cormier <m.cormier@vehikl.com>
2021-03-11 10:20:27 -0500

    Fixes bug with accessing env in vue

--- resources/js/components/views/WatcherIndex.vue
+++ resources/js/components/views/WatcherIndex.vue
@@ -180,8 +180,8 @@ export default {
     mounted() {
         this.watchersList = this.watchers;
 
-        const pusher = new Pusher(process.env.PUSHER_APP_KEY, {
-            cluster: process.env.PUSHER_APP_CLUSTER
+        const pusher = new Pusher(process.env.MIX_PUSHER_APP_KEY, {
+            cluster: process.env.MIX_PUSHER_APP_CLUSTER
         });
         const channel = pusher.subscribe(`user.${this.userId}.watchers`);
         channel.bind('update', (data) => {
