7d4f87a957abff3260eea49429d74c86c0efb5d5
Brian Hogg <brian@bhconsulting.ca>
2021-03-28 11:38:00 -0400

    Removing import

--- tests/Feature/TwilioServiceTest.php
+++ tests/Feature/TwilioServiceTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
 use NotificationChannels\Twilio\TwilioConfig;
 use Tests\TestCase;
