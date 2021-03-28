852f5e081ca6099619401bafb5c7f96d55a05953
Brian Hogg <brian@bhconsulting.ca>
2021-03-28 10:39:20 -0400

    Removing unused functions from notification alerts

--- app/Notifications/PriceAlert.php
+++ app/Notifications/PriceAlert.php
@@ -4,7 +4,6 @@ namespace App\Notifications;
 
 use App\Watcher;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use NotificationChannels\Pushover\PushoverChannel;
 use NotificationChannels\Pushover\PushoverMessage;
@@ -45,32 +44,5 @@ class PriceAlert extends Notification
                               ->title('Price Alert!')
                               ->sound('cashregister')
                               ->url($this->watcher->url);
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
     }
 }
