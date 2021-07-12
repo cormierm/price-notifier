bc190949d8d5269bdd0ec7169a5ec52e88118b68
Matt Cormier <m.cormier@vehikl.com>
2021-07-12 16:40:57 -0400

    Update seeders for laravel 8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(IntervalSeeder::class);
+        $this->call([
+            IntervalSeeder::class
+        ]);
     }
 }
