e81f2d4e359d43625e78430c852eed954e935521
Matt Cormier <cormierm@users.noreply.github.com>
2024-10-03 22:40:51 -0400

    Styling tweaks

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -20,7 +20,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
+    <div class="min-h-screen">
         @include('layouts.navigation')
 
         @isset($header)
