313f07eed19bb3cd3e24c541b5394fe0e93d0915
Matt Cormier <cormierm@users.noreply.github.com>
2024-10-03 22:36:52 -0400

    Upgrade Domain Query form to Vue 3

--- resources/vue/Pages/Template/Form.vue
+++ resources/vue/Pages/Template/Form.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white p-4">
-        <h1 class="text-xl">{{type}} Domain Query</h1>
+        <h1 class="text-xl">{{ type }} Domain Query</h1>
         <form class="mt-8" action="">
             <form-input
                 class="mt-4"
@@ -54,7 +54,7 @@
                 label="Stock Query"
                 placeholder="//span[@id='stock']"
                 :errors="formErrors['xpath_stock']"
-                v-model="xpathStock"
+                v-model="stockQuery"
             >
                 <div class="flex gap-3 pb-1">
                     <label class="flex items-center gap-2">
@@ -88,7 +88,7 @@
             </form-input>
 
             <div class="mt-4 flex items-center">
-                <select class="rounded" placeholder="Select stock condition" v-model="stockCondition">
+                <select class="rounded" v-model="stockCondition">
                     <option
                         v-for="option in stockConditions"
                         :value="option.value"
@@ -136,114 +136,100 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import FormInput from "@Components/Form/FormInput.vue";
+import {onMounted, ref} from "vue";
 
-export default {
-    name: "template-form",
-    components: {FormInput},
-    props: {
-        template: {
-            type: Object,
-            default: null,
-        },
-        type: {
-            type: String,
-            default: 'Create',
-        }
+const props = defineProps({
+    template: {
+        type: Object,
+        default: null,
     },
-    mounted() {
-        if (this.template) {
-            this.id = this.template.id;
-            this.domain = this.template.domain;
-            this.priceQuery = this.template.price_query;
-            this.priceQueryType = this.template.price_query_type;
-            this.client = this.template.client;
-            this.xpathStock = this.template.stock_query;
-            this.stockQueryType = this.template.stock_query_type;
-            this.stockText = this.template.stock_text;
-            this.stockCondition = this.template.stock_condition;
-        }
-    },
-    data() {
-        return {
-            loading: false,
-            id: null,
-            domain: '',
-            priceQuery: '//span[@id="price"]',
-            priceQueryType: 'xpath',
-            client: 'browsershot',
-            formErrors: {},
-            xpathStock: '',
-            stockCondition: 'contains_text',
-            stockText: '',
-            stockConditions: [
-                {label: 'Contains Text', value: 'contains_text'},
-                {label: 'Missing Text', value: 'missing_text'},
-                {label: 'Contains Html', value: 'contains_html'},
-                {label: 'Missing Html', value: 'missing_html'},
-            ],
-        };
-    },
-    methods: {
-        submit() {
-            this.loading = true;
-
-            if (this.id) {
-                this.update();
-            } else {
-                this.create();
-            }
-
-            this.loading = false;
-        },
-        create() {
-            axios.post('/template', {
-                domain: this.domain,
-                price_query: this.priceQuery,
-                price_query_type: this.priceQueryType,
-                client: this.client
-            }).then(() => {
-                window.location = '/template';
-            }).catch((err) => {
-                if (err.response.status === 422) {
-                    this.formErrors = err.response.data.errors;
-                } else {
-                    this.$buefy.toast.open({
-                        duration: 5000,
-                        message: err,
-                        type: 'is-danger'
-                    });
-                    console.error(err);
-                }
-            });
-        },
-        update() {
-            axios.put(`/template/${this.id}`, {
-                id: this.id,
-                domain: this.domain,
-                price_query: this.priceQuery,
-                price_query_type: this.priceQueryType,
-                client: this.client,
-                xpath_stock: this.xpathStock,
-                stock_query_type: this.stockQueryType,
-                stock_text: this.stockText,
-                stock_condition: this.stockCondition,
-            }).then(() => {
-                window.location = '/template';
-            }).catch((err) => {
-                if (err.response.status === 422) {
-                    this.formErrors = err.response.data.errors;
-                } else {
-                    this.$buefy.toast.open({
-                        duration: 5000,
-                        message: err,
-                        type: 'is-danger'
-                    });
-                    console.error(err);
-                }
-            });
-        }
+    type: {
+        type: String,
+        default: 'Create',
     }
-}
+});
+
+onMounted(() => {
+    if (props.template) {
+        id.value = props.template.id;
+        domain.value = props.template.domain;
+        priceQuery.value = props.template.price_query;
+        priceQueryType.value = props.template.price_query_type;
+        client.value = props.template.client;
+        stockQuery.value = props.template.stock_query;
+        stockQueryType.value = props.template.stock_query_type;
+        stockText.value = props.template.stock_text;
+        stockCondition.value = props.template.stock_condition;
+    }
+});
+
+const loading = ref(false);
+const id = ref(null);
+const domain = ref('');
+const priceQuery = ref('//span[@id="price"]');
+const priceQueryType = ref('xpath');
+const client = ref('browsershot');
+const formErrors = ref({});
+const stockQuery = ref('');
+const stockQueryType = ref('xpath');
+const stockCondition = ref('contains_text');
+const stockText = ref('');
+const stockConditions = ref([
+    {label: 'Contains Text', value: 'contains_text'},
+    {label: 'Missing Text', value: 'missing_text'},
+    {label: 'Contains Html', value: 'contains_html'},
+    {label: 'Missing Html', value: 'missing_html'},
+]);
+const submit = () => {
+    loading.value = true;
+
+    if (id.value) {
+        update();
+    } else {
+        create();
+    }
+
+    loading.value = false;
+};
+
+const create = () => {
+    axios.post('/template', {
+        domain: domain.value,
+        price_query: priceQuery.value,
+        price_query_type: priceQueryType.value,
+        client: client.value
+    }).then(() => {
+        window.location = '/template';
+    }).catch((err) => {
+        if (err.response.status === 422) {
+            formErrors.value = err.response.data.errors;
+        } else {
+            console.error(err);
+        }
+    });
+};
+
+const update = () => {
+    axios.put(`/template/${id.value}`, {
+        id: id.value,
+        domain: domain.value,
+        price_query: priceQuery.value,
+        price_query_type: priceQueryType.value,
+        client: client.value,
+        stock_query: stockQuery.value,
+        stock_query_type: stockQueryType.value,
+        stock_text: stockText.value,
+        stock_condition: stockCondition.value,
+    }).then(() => {
+        window.location = '/template';
+    }).catch((err) => {
+        if (err.response.status === 422) {
+            formErrors.value = err.response.data.errors;
+        } else {
+            console.error(err);
+        }
+    });
+};
 </script>
